28f28f66e3532a1c3836c9787677b5dbf73c6de6
Moalaa17V <124233979+Moalaa17V@users.noreply.github.com>
2023-06-30 22:46:35 +0300

    Update find_unique_nodes.py

--- find_unique_nodes.py
+++ find_unique_nodes.py
@@ -1,4 +1,7 @@
 import pandas as pd
+
+def find_unique_nodes(ppi_file)
+
 myfile =r'E:\Mohamed_R\Python_project\Script\PathLinker_2018_human-ppi-weighted-cap0_75.txt'
 df = pd.read_csv(myfile, sep='\t',header=(0))
 
