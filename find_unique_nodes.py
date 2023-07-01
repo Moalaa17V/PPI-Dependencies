44dbf95797e98b97fdb8b605d65aa0ba267ce2da
Moalaa17V <124233979+Moalaa17V@users.noreply.github.com>
2023-07-01 06:48:58 +0300

    Update find_unique_nodes.py

--- find_unique_nodes.py
+++ find_unique_nodes.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-def find_unique_nodes(ppi_file)
+def find_unique_nodes(ppi_file):
 
-myfile =r'E:\Mohamed_R\Python_project\Script\PathLinker_2018_human-ppi-weighted-cap0_75.txt'
-df = pd.read_csv(myfile, sep='\t',header=(0))
+    myfile =ppi_file
+    df = pd.read_csv(myfile, sep='\t',header=(0))
 
-a = df['#tail'].append(df['head']).unique()
-print(a)
+    a = df['#tail'].append(df['head']).nunique()
+    return find_unique_nodes
+    
+  
