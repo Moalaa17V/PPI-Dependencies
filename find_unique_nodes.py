7b6056341a7741c8a3e5278a9850fd3ab4376278
Moalaa17V <124233979+Moalaa17V@users.noreply.github.com>
2023-07-01 06:51:05 +0300

    Update find_unique_nodes.py

--- find_unique_nodes.py
+++ find_unique_nodes.py
@@ -2,10 +2,9 @@ import pandas as pd
 
 def find_unique_nodes(ppi_file):
 
-    myfile =ppi_file
+    myfile = ppi_file
     df = pd.read_csv(myfile, sep='\t',header=(0))
-
     a = df['#tail'].append(df['head']).nunique()
-    return find_unique_nodes
+    print(a)
     
-  
+    return find_unique_nodes
